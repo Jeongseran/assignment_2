953c1538b9f0d22fa2cf1fd2010b25b7a5c437f9
Jeongseran <69189965+Jeongseran@users.noreply.github.com>
2020-08-20 19:26:19 +0900

    Add files via upload

---
+++ ex06/sam_yook_gu.py
@@ -0,0 +1,21 @@
+Python 3.7.7 (default, May  6 2020, 11:45:54) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n = int(input())
+4
+>>> if n%3 == 2:
+	print('짝')
+else :
+	print(n+1)
+
+	
+5
+>>> n = int(input())
+8
+>>> if n%3 == 2:
+	print('짝')
+else :
+	print(n+1)
+
+	
+짝
+>>> 
